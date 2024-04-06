a6f604c3c126ddf8cff137445d67507248e27ef9
abhinav gandham <abhinavgandham@gmail.com>
2024-04-06 15:52:37 +1000

    Imported reactstrap and working on navbar

--- Volcanoes/src/App.jsx
+++ Volcanoes/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Navbar, NavItem, NavLink } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <NavigationBar />
+    </div>
+  );
+}
+
+function NavigationBar() {
+  return (
+    <Navbar>
+      <NavItem>
+        <NavLink>Home</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink>Volcano List</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink>Register</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink>Login</NavLink>
+      </NavItem>
+    </Navbar>
   );
 }
 
